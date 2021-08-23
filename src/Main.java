3efa1c221f1fc628ff954d6021b9c53cc176dd44
[Omar Khaled Mohy] <[omarkhaledm21@gmail.com]>
2021-08-24 01:37:02 +0200

    project files

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,8 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
 
-
-
-
         input.close();
     }
+
+
 }
--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,8 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class SortingAlgorithms {
 
     public static void bubbleSort(int[] arr,int size){
-        printArray(arr,size);
         for(int i=size-1; i>1; i--){
             for(int j=0; j<i; j++){
                 if(arr[j]>arr[j+1]){
@@ -14,6 +16,19 @@ public class SortingAlgorithms {
         }
     }
 
+    public static void selectionSort(int[] arr,int size){
+        int minInd=0;
+        for(int i = 0,j; i<size-1; i++){
+            for(j=i+1,minInd=i; j<size; j++){
+                if(arr[j]<arr[minInd]){
+                    minInd=j;
+                }
+            }
+            int temp = arr[minInd];
+            arr[minInd]=arr[i];
+            arr[i]=temp;
+        }
+    }
 
 
 
@@ -22,5 +37,11 @@ public class SortingAlgorithms {
             System.out.print(arr[i]+" ");
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={1,6,2,9,7,3};
+        selectionSort(arr,6);
+        System.out.println(Arrays.toString(arr));
     }
 }
